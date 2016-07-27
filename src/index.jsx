90a169881fc19d960d0a7a5be16ebcacef942558
stvking <tgfbikes@gmail.com>
2016-07-26 20:45:57 -0600

    changed to jsx extension

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes.jsx';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
