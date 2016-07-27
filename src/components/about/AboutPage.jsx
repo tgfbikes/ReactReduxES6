a193d0c15136098bef458c81434e38957d9b0973
stvking <tgfbikes@gmail.com>
2016-07-26 20:44:24 -0600

    initial static components to test routing

---
+++ src/components/about/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>About things</h1>
+        <p>I am doing things with React, Redux, and ES6</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
