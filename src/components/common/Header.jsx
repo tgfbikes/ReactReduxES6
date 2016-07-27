e38fb4e1b482ab27ad2b39eb233ec9df27996b15
stvking <tgfbikes@gmail.com>
2016-07-26 21:00:20 -0600

    created header component and imported to app

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div>
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <Link to="/" className="navbar-brand">Things</Link>
+            <ul className="nav navbar-nav">
+              <li><Link to="about">About</Link></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
