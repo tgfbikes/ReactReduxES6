c8d3b74e787ef8387fc55c7139a254ad87ea4398
stvking <tgfbikes@gmail.com>
2016-07-26 20:43:18 -0600

    changed to .jsx extension

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,8 +11,11 @@ export default {
     // Order is critical
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    './src/index' // Actual entry point has to be last
+    './src/index.jsx' // Actual entry point has to be last
   ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   target: 'web',
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
@@ -28,7 +31,7 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /\.js[x]?$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
       {test: /(\.css)$/, loaders: ['style', 'css']},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
